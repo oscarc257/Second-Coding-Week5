e434384a4ea7c3441eca2c5a2aa8b1f8a52dd7b8
Oscar C <oscar.carrillo758@gmail.com>
2022-10-08 16:23:27 -0700

    Update AsteriskLogger.java

--- Week 5 Coding Assignment/src/AsteriskLogger.java
+++ Week 5 Coding Assignment/src/AsteriskLogger.java
@@ -5,16 +5,16 @@
 
 
 
-
+//3. a. class implemented for Logger interface.
 
 public class AsteriskLogger implements Logger {
 
-	
+	//4. printed out asterisks before and after ***Log.***
 	public void Log(String str) {
 		System.out.println("***" + str + "***");
 	}
 
-	
+	// 5. printed out the asterisks above and below Error.
 	public void Error(String str) {
 		String mistake = "Error: " + str;
 		System.out.println("***********");
