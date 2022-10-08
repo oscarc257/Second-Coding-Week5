e47665c059ae058570338c4be527982b9de4b64e
Oscar C <oscar.carrillo758@gmail.com>
2022-10-08 16:26:02 -0700

    Update App.java

--- Week 5 Coding Assignment/src/App.java
+++ Week 5 Coding Assignment/src/App.java
@@ -1,11 +1,14 @@
+//10. instantiated an instance between both classes part of Logger interface.
 
 public class App {
+	
 
 	public static void main(String[] args) {
 		
 		  Logger asteriskLogger = new AsteriskLogger();
 	        Logger spacedLogger = new SpacedLogger();
 
+//11. 	Implemented  strings to test code.  		
 	        asteriskLogger.Log("Help!");
 	        asteriskLogger.Error("Me!");
 
