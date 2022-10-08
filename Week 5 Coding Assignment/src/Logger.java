70a23d9dfee770b40d0174ce6b71902154c1bae1
Oscar C <oscar.carrillo758@gmail.com>
2022-10-08 16:21:53 -0700

    Update Logger.java

--- Week 5 Coding Assignment/src/Logger.java
+++ Week 5 Coding Assignment/src/Logger.java
@@ -1,6 +1,8 @@
-
+//1. =====================
 public interface Logger {
-
+	
+//2. created interface named Logger
+	// Created two strings to determine Log an Error.
 	public void Log(String str);
     public void Error(String str);
 	
